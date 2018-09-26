39d6362867da671c963373fdc4e0e46080f322d1
pvedrine <vedrine.p@gmail.com>
2018-09-26 21:57:06 +0200

    Update README

--- AgeOf.py
+++ AgeOf.py
@@ -3,33 +3,38 @@
 
 import random
 
-lst = []
-# On prend les noms en entrée, séparés par des espaces
-chaps = [x for x in input("Donne les noms:\n").split()]
 
-# Shuffle change de place les données dans la liste aléatoirement
-random.shuffle(chaps)
-taille_chaps = len(chaps)
-# Liste des couleurs des ordis a rajouter pour compléter (toujours 8 joueurs)
-tbl = ["Bleu", "Rouge", "Vert", "Jaune", "Cyan", "Rose", "Gris ", "Orange"]
-taille = (len(tbl))
+def shuffle(liste):
+    tmp = list()
+    # Shuffle change de place les données dans la liste aléatoirement
+    random.shuffle(liste)
+    for i in range(0, len(liste)):
+        rand_elt = random.sample(liste, 1)  # On prend un nom aléatoire parmi les entrées
+        tmp.append(rand_elt[0])  # Ajout à un deuxieme index
+        liste.remove(rand_elt[0])  # Suppression de la premiere liste (sans remise)
+    return tmp
 
-for i in range(0, len(chaps)):  # Minimum 2 joueurs
-    rand0 = random.sample(chaps, 1)  # On prend un nom aléatoire parmi les entrées
-    lst.append(rand0[0])  # Ajout à un deuxieme index
-    chaps.remove(rand0[0])  # Suppression de la premiere liste (sans remise)
 
-for i in range(taille_chaps, taille):
-    lst.append("Ordi")  # On remplit avec des ordis
+if __name__ == '__main__':
+    # Liste des couleurs des ordis a rajouter pour compléter (toujours 8 joueurs)
+    tbl = ["Bleu", "Rouge", "Vert", "Jaune", "Cyan", "Rose", "Gris ", "Orange"]
 
-# On double l'aléatoire
-lst_coul = list()
-for m in range(0, taille):
-    rand = random.sample(tbl, 1)  # prends une couleur aléatoire dans la liste
-    lst_coul.append(rand[0])
+    # On prend les noms en entrée, séparés par des espaces
+    chaps = [x for x in input("Donne les noms:\n").split()]
+    taille_chaps = len(chaps)
 
-final = ""
-for i in range(len(tbl)):
-    print(str(lst_coul[i]) + " -> " + str(lst[i]))
+    #  Maximum 8 joueurs
+    if len(chaps) > 8:
+        raise ValueError('Maximum 8 joueurs')
 
-input()
+    lst = shuffle(chaps)
+    for i in range(taille_chaps, len(tbl)):
+        lst.append("Ordi")  # On remplit avec des ordis
+
+    # On double l'aléatoire
+    lst_coul = shuffle(tbl)
+
+    # Affichage
+    for i in range(len(lst_coul)):
+        print(str(lst_coul[i]) + " -> " + str(lst[i]))
+    input()
